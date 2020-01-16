bca94d6bdf3317afa4bdb42faed454527fc50747
ClarisseU <clarisseum@gmail.com>
2020-01-16 17:01:32 +0200

    add different method

--- mathanapp/views.py
+++ mathanapp/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+import datetime as dt
+from django.http import HttpResponse, Http404
+from .models import Image,Category
 
 # Create your views here.
+def welcome(request):
+    images = Image.objects.all()
+    
+    return render(request, 'index.html',{'image':images})
+
+def search_results(request):
+    '''
+    a view to do the search the image by category
+    '''
+    if 'image' in request.GET and request.GET["image"]:
+        search = request.GET.get('image')
+        searched = Image.search_image(search)
+        message = f"{search}"
+        
+        return render(request,"all-photos/search.html", {"message":message, "images":searched})
+
+    else:
+        message = 'You have not searched for any image'
+        return render(request, 'all-photos/search.html',{"message":message})    
+
+def image(request,id):
+    try:
+        images= Image.objects.get(id = id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,'all-photos/image.html',{'image':images}) 
+
+def category(request,location):
+    image = Image.filter_by_location(location)
+    category = Category.objects.all()
+    return render(request,'all-photos/location.html',{'category':category, 'image':image})
