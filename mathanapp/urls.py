686d5cbc7f0f21f17f62d70b3e65acbbf604c545
ClarisseU <clarisseum@gmail.com>
2020-01-16 16:59:23 +0200

    add different regex

---
+++ mathanapp/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    
+    url(r'^$', views.welcome, name = 'welcome'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(?P<id>[\w-]+)/$', views.image, name='image'),
+    # url(r'^location/(?P<location>\D+)/$', views.location, name='location'),
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
