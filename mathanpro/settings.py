1c64f1b6e3bd809fed2767e8b46818cdb58e5535
ClarisseU <clarisseum@gmail.com>
2020-04-26 09:26:52 +0200

    finalize touch

--- mathanpro/settings.py
+++ mathanpro/settings.py
@@ -15,10 +15,6 @@ import django_heroku
 import dj_database_url
 from decouple import config,Csv
 
-MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -83,7 +79,10 @@ WSGI_APPLICATION = 'mathanpro.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+#development
 if config('MODE')=="dev":
    DATABASES = {
        'default': {
